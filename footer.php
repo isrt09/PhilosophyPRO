62416b7893ee3ca3c4a6f157235c2018819c1cbc
isrt09 <mazedur@gmail.com>
2021-01-17 12:30:01 +0600

    Social Widgets Added

--- footer.php
+++ footer.php
@@ -35,21 +35,7 @@
       <div class="col-four md-six tab-full about">
          <?php if ( is_active_sidebar( 'before-footer-right-section' ) ) { ?>             
             <?php dynamic_sidebar('before-footer-right-section'); ?>             
-         <?php } ?>
-         <ul class="about__social">
-            <li>
-               <a href="#0"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            </li>
-            <li>
-               <a href="#0"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </li>
-            <li>
-               <a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            </li>
-            <li>
-               <a href="#0"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-            </li>
-         </ul>
+         <?php } ?>         
          <!-- end header__social -->
       </div>
       <!-- end about -->
